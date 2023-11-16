3d5b6911b924cfce78f4f3bf8f59b4f1909465f8
Nicolas Bolañez <Zven94>
2023-11-16 20:26:12 -0300

    update border style

--- src/components/NavElement/NavElement.jsx
+++ src/components/NavElement/NavElement.jsx
@@ -1,18 +1,28 @@
 import './NavElement.css';
 import Container from 'react-bootstrap/Container';
-import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <>
-      <header className="header">
+      <header className="header navbar-container">
         <Container className="d-flex justify-content-center" bordered>
-          <Col className="d-flex justify-content-center">
-            <nav>
-              <h1 className="d-flex justify-content-center">Stock Prices</h1>
-              <h2 className="d-flex justify-content-center"> NYSE INDEX </h2>
-            </nav>
-          </Col>
+          <nav className="w-100">
+            <Row className="d-flex justify-content-end w-100 mt-3">
+              <Link to="/">
+                <button className="watch-list" type="submit">
+                  <i className="bi bi-eye" />
+                </button>
+              </Link>
+              <Link to="/">
+                <button className="about-us" type="submit">About Us</button>
+              </Link>
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <h2> NYSE INDEX </h2>
+            </Row>
+          </nav>
         </Container>
       </header>
     </>
