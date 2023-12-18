52c05fb278a85287826d1e186d2abf946dbfed21
Nicolas Bolañez <Zven94>
2023-12-18 20:55:06 -0300

    updated detailpage code and style

--- src/router/Detailpage.jsx
+++ src/router/Detailpage.jsx
@@ -1,90 +1,90 @@
 import { Link, useParams } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
 import Navbar from '../components/NavElement/NavElement';
+import './Detailpage.css';
 
 export default function Detailpage() {
   const {
     symbol, name, price, change, dayLow, dayHigh, marketCap, volume, exchange,
   } = useParams();
   return (
-    <main>
+    <main className="main-with-background">
       <Navbar />
       <Container
-        className="d-flex justify-content-center align-items-center"
+        className="d-flex justify-content-center align-items-center stock-container"
         style={{
-          height: '80vh', backgroundColor: 'black', color: 'white', fontSize: '2rem',
+          width: '45vw', height: '80vh', color: 'white', fontSize: '1.75vw',
         }}
       >
-        <Col md={8} className=" align-items-center">
-          <Row className="d-flex justify-content-center">
+        <Col md={8} className="d-flex flex-column justify-content-center align-items-start">
+          <Row className="d-flex justify-content-center mb-2">
             <h2>Stock Detail</h2>
           </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
+          <Row className="d-flex justify-content-left align-items-center">
+            <p className="pDetail">
               Symbol:
               {' '}
               {symbol}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Name:
               {' '}
               {name}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Price:
               {' '}
               {price}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Daily change:
               {' '}
               {change}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Min value today:
               {' '}
               {dayLow}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Max value today:
               {' '}
               {dayHigh}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Market Cap:
               {' '}
               {marketCap}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Vol:
               {' '}
               {volume}
             </p>
           </Row>
           <Row className="d-flex justify-content-center">
-            <p>
+            <p className="pDetail">
               Exchange:
               {' '}
               {exchange}
             </p>
           </Row>
-          <Link to="/" className="d-flex justify-content-center">
-            <Button type="submit" variant="secondary" className="mt-3"> &crarr; Close</Button>
+          <Link to="/homepage" className="d-flex justify-content-center">
+            <button type="submit" className="mt-3 closeButton"> &crarr; </button>
           </Link>
         </Col>
       </Container>
