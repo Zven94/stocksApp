4f061877819573dbb7942f3af3f1f7aefd14d3c0
Nicolas Bolañez <Zven94>
2023-12-18 20:54:52 -0300

    updated homepage code and style

--- src/router/Homepage.jsx
+++ src/router/Homepage.jsx
@@ -1,14 +1,18 @@
 import './Homepage.css';
 import { Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/NavElement/NavElement';
 import DisplayStocks from '../components/displayData/displayStocks';
 
 export default function Homepage() {
   return (
-    <main className="main-with-background mi-contenedor-con-max-width">
+    <main className="d-flex main-with-background mi-contenedor-con-max-width">
       <Col className="justify-content-center">
         <Navbar />
         <DisplayStocks />
+        <Link to="/" className="d-flex justify-content-center">
+          <button type="submit" className="mt-3 closeButtonH"> &crarr; </button>
+        </Link>
       </Col>
     </main>
   );
