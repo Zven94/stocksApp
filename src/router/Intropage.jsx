9d5b1d0f1f004653b7ee60f4af1a228f2070a938
Nicolas Bolañez <Zven94>
2023-12-18 20:54:32 -0300

    updated intropage

---
+++ src/router/Intropage.jsx
@@ -0,0 +1,102 @@
+import { Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Homepage.css';
+
+export default function Intropage() {
+  return (
+    <main className="main-with-background mi-contenedor-con-max-width d-flex flex-column align-items-center">
+      <Row className="d-flex flex-column justify-content-center align-items-center title-container">
+        <h2 className="h2-editor">Index Dashboard</h2>
+      </Row>
+      <div className="d-flex flex-column justify-content-center align-items-center stock-container">
+        <Row>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/homepage">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">Crypto INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+        </Row>
+        <Row>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+        </Row>
+        <Row>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+          <article className="card-with-background d-flex flex-column justify-content-center align-items-center">
+            <Link to="/coomingsoon">
+              <button className="index-button" type="submit">NYSE INDEX</button>
+            </Link>
+            <p className="p-1 pTagcontenedor">
+              in this index you can find stocks like: apple,meta,alphabet,
+              etc...
+            </p>
+          </article>
+        </Row>
+      </div>
+    </main>
+  );
+}
