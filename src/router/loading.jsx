f14f406a0dacd31a1287a3f1a8aae4e1bad478c2
Nicolas Bolañez <Zven94>
2023-12-26 16:41:31 -0300

    fixed styles for loading component

--- src/router/loading.jsx
+++ src/router/loading.jsx
@@ -4,7 +4,7 @@ import './loading.css';
 function Loading() {
   return (
     <>
-      <div className="spinner-grow d-flex align-items-start" role="status">
+      <div className="spinner-grow d-flex justify-conent-around align-items-start" role="status">
         <h1 className="ml-5 mb-0 h1-spinner">Loading...</h1>
       </div>
     </>
