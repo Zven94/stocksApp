36927bcdf5721d7094a2e3943452c4d499dd0ce0
Nicolas Bolañez <Zven94>
2023-12-26 17:05:58 -0300

    added Loading component to Home Page

--- src/router/Homepage.jsx
+++ src/router/Homepage.jsx
@@ -1,18 +1,34 @@
 import './Homepage.css';
 import { Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/NavElement/NavElement';
 import DisplayStocks from '../components/displayData/displayStocks';
+import Loading from './loading';
 
 export default function Homepage() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, [1000]);
+  }, []);
   return (
     <main className="d-flex main-with-background mi-contenedor-con-max-width">
       <Col className="justify-content-center">
         <Navbar />
-        <DisplayStocks />
-        <Link to="/" className="d-flex justify-content-center">
-          <button type="submit" className="mt-3 closeButtonH"> &crarr; </button>
-        </Link>
+        {loading
+          ? <Loading loading={loading} />
+          : (
+            <>
+              <DisplayStocks />
+              <Link to="/" className="d-flex justify-content-center">
+                <button type="submit" className="mt-3 closeButtonH"> &crarr; </button>
+              </Link>
+            </>
+          )}
       </Col>
     </main>
   );
